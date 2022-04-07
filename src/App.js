5c72332bea87412eef15596738df63a549e4d1a7
Giorgi-dl1 <96057077+Giorgi-dl1@users.noreply.github.com>
2022-04-07 21:48:34 +0400

    initialise project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import './app.css'
+import Todo from './components/Todo';
 function App() {
+  const [light,setLight] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={light ? 'App light':'App'}>
+      <div className="background-img"/>
+      <div className="todo-container">
+        <Todo/>
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/Todo.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react'
+import List from './List'
+import sun from '../images/icon-sun.svg'
+
+function Todo() {
+    const [data,setData] = useState([
+        {text:'add light mode',completed:false},
+        {text:'make todo list',completed:true},
+        {text:'my list is sortable',completed:false}
+    ]);
+    const [activeData,setActiveData]=useState(data);
+    const [input,setInput] = useState('');
+    const [filter,setFilter] = useState('all');
+    const setCompleted = (i)=>{
+       let newData=data.map((item,index)=>{
+            if(index == i ){
+                return {...item,completed:true}
+            }
+            return item
+       })
+       setData(newData);
+       setActiveData(newData)
+    }
+    const deleteTodo = (text)=>{
+        let newData = data.filter((item)=>item.text !== text);
+        setData(newData);
+        setActiveData(newData)
+    }
+    const addTodo =(e)=>{
+        if(e.keyCode === 13){
+            e.preventDefault();
+            if(input.length>=1){
+                let newObj = {text:input,completed:false};
+                setData([...data,newObj]);
+                setActiveData([...data,newObj])
+
+            }
+            setInput('');
+        }
+    }
+    const filterData =(type)=>{
+        if(type==='all'){
+            setActiveData(data);
+        }else if(type === 'active'){
+            setActiveData(data.filter(item => !item.completed))
+        }else if(type === 'completed'){
+            setActiveData(data.filter(item => item.completed))
+        }
+        setFilter(type);
+    }
+    const clearCompleted =()=>{
+        setData(data.filter(item=>!item.completed));
+        setActiveData(data.filter(item=>!item.completed));
+        setFilter('all')
+    }
+    return (
+        <div className="todo">
+
+            <div className="todo-header">
+                <h1>TODO</h1>
+                <div className="icon">
+                    <img src={sun} alt="icon" />
+                </div>
+            </div>
+            <div className="input">
+                <div className="circle"></div>
+                <input 
+                type="text" 
+                placeholder='Create a new todo...'
+                value={input}
+                onChange={(e)=>setInput(e.target.value)}
+                onKeyDown={addTodo}
+                />
+            </div>
+            
+            <List data={activeData} 
+            setCompleted={setCompleted}
+            deleteTodo={deleteTodo}
+            filterData={filterData}
+            clearCompleted={clearCompleted}
+            filter={filter}
+            />
+            
+        </div>
+  )
+}
+
+export default Todo
