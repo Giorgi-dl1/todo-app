87a1a0937fea88538d83d2f18ebd2d56c467da40
Giorgi-dl1 <96057077+Giorgi-dl1@users.noreply.github.com>
2022-09-22 12:04:53 +0400

    fix terminal issues

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,50 +1,77 @@
-import React, { useEffect, useState } from 'react'
-import deleteBtn from '../images/icon-cross.svg'
-import checkIcon from '../images/icon-check.svg'
-function List({data,setCompleted,deleteTodo,filterData,clearCompleted,filter}) {
-   
+import deleteBtn from "../images/icon-cross.svg";
+import checkIcon from "../images/icon-check.svg";
+function List({
+  data,
+  setCompleted,
+  deleteTodo,
+  filterData,
+  clearCompleted,
+  filter,
+}) {
   return (
-    <div className='todos'>
-        <div className="todos-cont">
-            {data.length==0
-            ? 
-            <div className='no-todos'>No todos left</div>
-            :
-            data.map((item,index)=>{
-                return(
-                    <div key={index} className={item.completed?'item completed':'item active'}>
-                        <div className="circle-text">
-                            <div className="circle" onClick={()=>setCompleted(item.text)}>
-                                {item.completed && <img src={checkIcon} alt='check icon'/>}
-                            </div>
-                            <p onClick={()=>setCompleted(index)}>{item.text}</p>
-                        </div>
-                        {!item.completed
-                        &&
-                            <div className="delete-btn" onClick={()=>deleteTodo(index)}>
-                                <img src={deleteBtn} alt="delete icon" />
-                            </div>
-                        }
-                        
-                    </div>
-                )
-            })
-            }
+    <div className="todos">
+      <div className="todos-cont">
+        {data.length === 0 ? (
+          <div className="no-todos">No todos left</div>
+        ) : (
+          data.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className={item.completed ? "item completed" : "item active"}
+              >
+                <div className="circle-text">
+                  <div
+                    className="circle"
+                    onClick={() => setCompleted(item.text)}
+                  >
+                    {item.completed && <img src={checkIcon} alt="check icon" />}
+                  </div>
+                  <p onClick={() => setCompleted(index)}>{item.text}</p>
+                </div>
+                {!item.completed && (
+                  <div className="delete-btn" onClick={() => deleteTodo(index)}>
+                    <img src={deleteBtn} alt="delete icon" />
+                  </div>
+                )}
+              </div>
+            );
+          })
+        )}
+      </div>
+
+      <footer>
+        <div className="items-left">
+          <p>
+            {data.filter((item) => item.completed === false).length} items left
+          </p>
         </div>
-        
-        <footer>
-            <div className="items-left">
-                <p>{data.filter(item => item.completed ==false).length} items left</p>
-            </div>
-            <div className="filter">
-                <p className={filter=='all'?'action choice':'action'} onClick={()=>filterData('all')}>All</p>
-                <p className={filter=='active'?'action choice':'action'} onClick={()=>filterData('active')}>Active</p>
-                <p className={filter=='completed'?'action choice':'action'} onClick={()=>filterData('completed')}>Completed</p>
-            </div>
-            <p className='action' onClick={()=>clearCompleted()}>Clear completed</p>
-        </footer>
+        <div className="filter">
+          <p
+            className={filter === "all" ? "action choice" : "action"}
+            onClick={() => filterData("all")}
+          >
+            All
+          </p>
+          <p
+            className={filter === "active" ? "action choice" : "action"}
+            onClick={() => filterData("active")}
+          >
+            Active
+          </p>
+          <p
+            className={filter === "completed" ? "action choice" : "action"}
+            onClick={() => filterData("completed")}
+          >
+            Completed
+          </p>
+        </div>
+        <p className="action" onClick={() => clearCompleted()}>
+          Clear completed
+        </p>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,106 +1,114 @@
-import React, { useEffect, useState } from 'react'
-import List from './List'
-import sun from '../images/icon-sun.svg'
-import moon from '../images/icon-moon.svg'
+import { useState } from "react";
+import List from "./List";
+import sun from "../images/icon-sun.svg";
+import moon from "../images/icon-moon.svg";
 
-function Todo({light,setLight}) {
-    const [data,setData] = useState([
-        {text:'my app has night mode',completed:true},
-        {text:'this todo app is responsive',completed:true},
-        {text:'my list is sortable',completed:false}
-    ]);
-    const [activeData,setActiveData]=useState(data);
-    const [input,setInput] = useState('');
-    const [filter,setFilter] = useState('all');
-    const setCompleted = (i)=>{
-       let newData=data.map((item)=>{
-            if(item.text == i ){
-                console.log({...item,completed:true})
-                return {...item,completed:true}
-            }
-            console.log(item,'ch')
-            return item
-       })
-       
-       setData(newData);
-       if(filter == 'active'){
-            setActiveData(newData.filter(item => !item.completed));
-       }else if(filter == 'completed'){
-           setActiveData(newData.filter(item => item.completed));
-       }else{
-           setActiveData(newData)
-       }
-       
-       
-    }
-    const deleteTodo = (text)=>{
-        let newData = data.filter((item,index)=>index !== text);
-        setData(newData);
-        setActiveData(newData)
-    }
-    const addTodo =(e)=>{
-        if(e.keyCode === 13){
-            e.preventDefault();
-            if(input.length>=1){
-                let newObj = {text:input,completed:false};
-                setData([...data,newObj]);
-                setActiveData([...data,newObj])
+function Todo({ light, setLight }) {
+  const [data, setData] = useState([
+    { text: "my app has night mode", completed: true },
+    { text: "this todo app is responsive", completed: true },
+    { text: "my list is sortable", completed: false },
+  ]);
+  const [activeData, setActiveData] = useState(data);
+  const [input, setInput] = useState("");
+  const [filter, setFilter] = useState("all");
+  const setCompleted = (i) => {
+    let newData = data.map((item) => {
+      if (item.text === i) {
+        return { ...item, completed: true };
+      }
+      return item;
+    });
 
-            }
-            setInput('');
-        }
+    setData(newData);
+    if (filter === "active") {
+      setActiveData(newData.filter((item) => !item.completed));
+    } else if (filter === "completed") {
+      setActiveData(newData.filter((item) => item.completed));
+    } else {
+      setActiveData(newData);
     }
-    const filterData =(type)=>{
-        if(type==='all'){
-            setActiveData(data);
-        }else if(type === 'active'){
-            setActiveData(data.filter(item => !item.completed))
-        }else if(type === 'completed'){
-            setActiveData(data.filter(item => item.completed))
-        }
-        setFilter(type);
+  };
+  const deleteTodo = (text) => {
+    let newData = data.filter((item, index) => index !== text);
+    setData(newData);
+    setActiveData(newData);
+  };
+  const addTodo = (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      if (input.length >= 1) {
+        let newObj = { text: input, completed: false };
+        setData([...data, newObj]);
+        setActiveData([...data, newObj]);
+      }
+      setInput("");
     }
-    const clearCompleted =()=>{
-        setData(data.filter(item=>!item.completed));
-        setActiveData(data.filter(item=>!item.completed));
-        setFilter('all')
-    }
-    return (
-        <div className="todo">
-
-            <div className="todo-header">
-                <h1>TODO</h1>
-                <div className="icon" onClick={()=>setLight(!light)}>
-                    <img src={light?moon:sun} alt="icon" />
-                </div>
-            </div>
-            <div className="input">
-                <div className="circle"></div>
-                <input 
-                type="text" 
-                placeholder='Create a new todo...'
-                value={input}
-                onChange={(e)=>setInput(e.target.value)}
-                onKeyDown={addTodo}
-                />
-            </div>
-            
-            <List data={activeData} 
-            setCompleted={setCompleted}
-            deleteTodo={deleteTodo}
-            filterData={filterData}
-            clearCompleted={clearCompleted}
-            filter={filter}
-            />
-            <div className="mobile-filter">
-                <div className="filter">
-                    <p className={filter=='all'?'action choice':'action'} onClick={()=>filterData('all')}>All</p>
-                    <p className={filter=='active'?'action choice':'action'} onClick={()=>filterData('active')}>Active</p>
-                    <p className={filter=='completed'?'action choice':'action'} onClick={()=>filterData('completed')}>Completed</p>
-                </div>
-            </div>
+  };
+  const filterData = (type) => {
+    type === "all"
+      ? setActiveData(data)
+      : type === "active"
+      ? setActiveData(data.filter((item) => !item.completed))
+      : setActiveData(data.filter((item) => item.completed));
+    setFilter(type);
+  };
+  const clearCompleted = () => {
+    setData(data.filter((item) => !item.completed));
+    setActiveData(data.filter((item) => !item.completed));
+    setFilter("all");
+  };
+  return (
+    <div className="todo">
+      <div className="todo-header">
+        <h1>TODO</h1>
+        <div className="icon" onClick={() => setLight(!light)}>
+          <img src={light ? moon : sun} alt="icon" />
         </div>
-  )
+      </div>
+      <div className="input">
+        <div className="circle"></div>
+        <input
+          type="text"
+          placeholder="Create a new todo..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={addTodo}
+        />
+      </div>
+
+      <List
+        data={activeData}
+        setCompleted={setCompleted}
+        deleteTodo={deleteTodo}
+        filterData={filterData}
+        clearCompleted={clearCompleted}
+        filter={filter}
+      />
+      <div className="mobile-filter">
+        <div className="filter">
+          <p
+            className={filter === "all" ? "action choice" : "action"}
+            onClick={() => filterData("all")}
+          >
+            All
+          </p>
+          <p
+            className={filter === "active" ? "action choice" : "action"}
+            onClick={() => filterData("active")}
+          >
+            Active
+          </p>
+          <p
+            className={filter === "completed" ? "action choice" : "action"}
+            onClick={() => filterData("completed")}
+          >
+            Completed
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Todo
+export default Todo;
